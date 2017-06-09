dd6ffbcc96e33a634a1a8bf06d54e7697020e6fb
MarticiousMarty <Martyna_Burrows@stu.aii.edu>
2017-06-08 17:04:08 -0700

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,6 @@
+def think_hard():
+    return 42
+
+class Bandit:
+    def __init__(self):
+        self.name = 'Marty'
---
+++ main.py
@@ -0,0 +1,7 @@
+import logic
+
+answer = logic.think_hard()
+robber = logic.Bandit()
+
+print(answer)
+print(robber.name)
